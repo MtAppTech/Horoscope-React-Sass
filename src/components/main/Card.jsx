d00e31689ba93418e0f23679078b123b65c0b555
murat <yazilimgenel@gmail.com>
2023-11-03 08:57:39 +0100

    mac

---
+++ src/components/main/Card.jsx
@@ -0,0 +1,23 @@
+import React from 'react'
+
+const Card = ({item}) => {
+    console.log(item);
+    return (
+        
+        <div className='cards'>
+            <div className="title">
+                <h1>{item.title}</h1>
+            </div>
+            <div className="date">
+                <h1>{item.date}</h1>
+            </div>
+            <img src={item.image} alt="" />
+            <div className="card-over">
+                <p>{item.desc}</p>
+            </div>
+
+        </div>
+    )
+}
+
+export default Card
